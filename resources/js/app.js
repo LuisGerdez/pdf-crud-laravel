439f96e6462bf3ab19ac2fc4a1e2196790a6e405
unknown <elcreidoluis@gmail.com>
2023-01-04 16:06:51 -0400

    Second commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,12 @@ window.$ = jQuery;
 
 import { valuesIn } from 'lodash';
 import './bootstrap';
+
+document.addEventListener('DOMContentLoaded', function() {
+    $('input[type="file"]').on('change', function() {
+        if(this.files[0].size > 5242880)    {
+            alert('This file size is too large. Max size is 5MB'); 
+            this.value = '';
+        }
+    });
+});
